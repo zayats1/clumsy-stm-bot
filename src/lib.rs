7f8c434e2c165756d6286162be97c84270c33b23
zayats1 <zaiats.bogdan@gmail.com>
2025-03-02 17:14:33 +0200

    move mocks to the unit test module

--- src/lib.rs
+++ src/lib.rs
@@ -15,40 +15,15 @@ use embassy_stm32 as _;
 
 use panic_probe as _;
 
-use embedded_hal::digital::ErrorKind;
-use embedded_hal::digital::{ErrorType, InputPin};
-
-#[derive(Default)]
-struct MockPin {
-    state: bool,
-}
-
-impl MockPin {
-    fn new(state: bool) -> Self {
-        Self { state }
-    }
-}
-
-impl ErrorType for MockPin {
-    type Error = ErrorKind;
-}
-
-impl InputPin for MockPin {
-    fn is_high(&mut self) -> Result<bool, Self::Error> {
-        Ok(self.state)
-    }
-
-    fn is_low(&mut self) -> Result<bool, Self::Error> {
-        Ok(!self.state)
-    }
-}
-
 // run with cargo test --lib
 #[cfg(test)]
 #[defmt_test::tests]
 mod unit_tests {
-    use super::*;
     use defmt::assert;
+
+    use embedded_hal::digital::ErrorKind;
+    use embedded_hal::digital::{ErrorType, InputPin};
+
     #[test]
     fn line_sensor() {
         use crate::drivers::line_sensor::LineSensor;
@@ -57,5 +32,30 @@ mod unit_tests {
         let new_pin = MockPin::new(false);
         let mut sensor2 = LineSensor::new(new_pin);
         assert!(!sensor2.is_on_line());
+    }
+
+    #[derive(Default)]
+    struct MockPin {
+        state: bool,
+    }
+
+    impl MockPin {
+        fn new(state: bool) -> Self {
+            Self { state }
+        }
+    }
+
+    impl ErrorType for MockPin {
+        type Error = ErrorKind;
+    }
+
+    impl InputPin for MockPin {
+        fn is_high(&mut self) -> Result<bool, Self::Error> {
+            Ok(self.state)
+        }
+
+        fn is_low(&mut self) -> Result<bool, Self::Error> {
+            Ok(!self.state)
+        }
     }
 }
